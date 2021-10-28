4e3caa4a4b86ceaaf7ba4332b3f3bf3ffd2faca7
Muriellehxh <dmui49@gmail.com>
2021-10-28 15:30:46 -0400

    ex1 moitié fait?

--- exercice.py
+++ exercice.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import json
 import os
 import sys
+
 
 # notes chap 8
 
@@ -12,29 +14,81 @@ import sys
 def read_text(path="./exemple.txt"):
     with open(path, "r", encoding='utf-8') as f:
         print(f.readlines())
-        f.seek(0)   # on recommence le reader à la premiere ligne
+        f.seek(0)  # on recommence le reader à la premiere ligne
         print(f.readlines()[0])
+
+
+def write_json(path="./jason.txt"):
+    couleur = {"lion": "ok"}
+
+
+# sauvegarder dict => UTILISER JSON, À EXAMEN!!!!!
+
+
+def write_pickle():
+    couleur = {"lion": "bleh"}
+
+
+# sauvegarder GROS fichier en binaire, diff avec json = pickle plus compact, tandis que json qd tu retourne dans json.txt,
+# json.txt garde texte original + modification
 
 
 if __name__ == '__main__':
     print(read_text("./exemple.txt"))
 
+    # on peut lire et écrire en mettant dans with open(path, "r+", etc)
+    # r+ = r et append (no squashing, old file is still there but u can write)
+    # r + w = squashes (writes new) and reads
+
+    print(os.getcwd())  # = get current working directory (find where ur file is)
+
+    sys.stdout = open('./file.txt', 'w')
+    print('test')
+    print('ecrit dans un fichier')
+    sys.stdout.close()
+
+# si on veut print un nouveau text file pour tester nos print dans un gros code
 
 
-# on peut lire et écrire en mettant dans with open(path, "r+", etc)
-# r+ = r et append (no squashing, old file is still there but u can write)
-# r + w = squashes (writes new) and reads
-
-
-PERCENTAGE_TO_LETTER = {"A*": [95, 101], "A": [90, 95], "B+": [85, 90], "B": [80, 85], "C+": [75, 80], "C": [70, 75], "F": [0, 70]}
+PERCENTAGE_TO_LETTER = {"A*": [95, 101], "A": [90, 95], "B+": [85, 90], "B": [80, 85], "C+": [75, 80], "C": [70, 75],
+                        "F": [0, 70]}
 
 # TODO: Importez vos modules ici
 
+import os
+import sys
 
 # TODO: Définissez vos fonction ici
+
+f1_list = []
+f2_list = []
+
+
+def comparaison_2_fichiers(f1, f2):
+    with open(f1, "r") as f1_read:
+        for line in f1_read:
+            for ch in line:
+                f1_list.append(ch)
+
+    with open(f2, "r") as f2_read:
+        for line in f2_read:
+            for ch in line:
+                f2_list.append(ch)
+
+    if f1_list == f2_list:
+        print('listes sont les memes')
+
+    for ch1 in f1_list:
+        for ch2 in f2_list:
+            if ch1 != ch2:
+                return ch1, ch2
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
+
+    f_1 = open("./fichier1", "r", encoding='utf-8')
+    f_2 = open("./fichiee2", "r", encoding='utf-8')
+    print(comparaison_2_fichiers(f_1, f_2))
 
     pass
