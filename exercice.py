18d090ce9f08529fe07432a557cfa4f7d8b68bab
Muriellehxh <dmui49@gmail.com>
2021-11-03 19:02:36 -0400

    commmit1

--- exercice.py
+++ exercice.py
@@ -9,6 +9,7 @@ import sys
 # notes chap 8
 
 # quand on lie document avec read(), le / est un char qui determine fin de ligne
+
 # le deuxieme print(readlines) continue de lire where the first one left off
 
 def read_text(path="./exemple.txt"):
@@ -53,6 +54,10 @@ if __name__ == '__main__':
 PERCENTAGE_TO_LETTER = {"A*": [95, 101], "A": [90, 95], "B+": [85, 90], "B": [80, 85], "C+": [75, 80], "C": [70, 75],
                         "F": [0, 70]}
 
+
+# end NOTES CHAP8
+
+
 # TODO: Importez vos modules ici
 
 import os
@@ -63,33 +68,37 @@ import sys
 f1_list = []
 f2_list = []
 
-
 def comparaison_2_fichiers(f1, f2):
-    with open(f1, "r") as f1_read:
+    with open(f1, 'r') as f1_read:
         for line in f1_read:
             for ch in line:
                 f1_list.append(ch)
 
-    with open(f2, "r") as f2_read:
-        for line in f2_read:
-            for ch in line:
-                f2_list.append(ch)
+    with open(f2, 'r') as f2_read:
+        for ligne in f2_read:
+            for mn in ligne:
+                f2_list.append(mn)
 
     if f1_list == f2_list:
         print('listes sont les memes')
 
-    for ch1 in f1_list:
-        for ch2 in f2_list:
-            if ch1 != ch2:
-                return ch1, ch2
-    
+
+    else:
+        if len(f1_list) < len(f2_list):
+            return f2_list[f1_list.index(f1_list[-1]) + 1]
+        if len(f2_list) < len(f1_list):
+            return f1_list[f2_list.index(f2_list[-1]) + 1]
+
+        if len(f1_list) == len(f2_list):
+            for n in range(len(f1_list)):
+                if f1_list[n] != f2_list[n]:
+                   return f1_list[n], f2_list[n]
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
 
-    f_1 = open("./fichier1", "r", encoding='utf-8')
-    f_2 = open("./fichiee2", "r", encoding='utf-8')
-    print(comparaison_2_fichiers(f_1, f_2))
+    f1 = 'fichier1.txt'
+    f2 = 'fichier2.txt'
+    print(comparaison_2_fichiers(f1, f2))
 
-    pass
